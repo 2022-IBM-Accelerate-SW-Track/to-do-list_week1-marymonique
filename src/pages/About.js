5c22ff352d4e27d14f685ebc24c3af6ab54d2a1f
marymonique <mary99campbell@gmail.com>
2022-06-15 14:48:52 -0700

    Adding bio and todo list feature

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profilepic3 from "../assets/profilepic3.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            // Image goes here
+            src= {profilepic3}
+            alt="Profile Pic"
+            ></img>
+        </div>
       </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Mary Campbell</div>
+          <div className="brief_description">
+            Hi! I'm Mary, I'm a student at the University of California, Irvine studying Computer Engineering
+            and I love coffee and being active.
+          </div>
+        </div>
+      </div>
+    </div>
     )
   }
 }
